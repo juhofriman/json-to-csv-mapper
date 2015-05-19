343c58498b30f31c0fe2f24b89b6d317a652c2c4
Juho Friman <juho.friman@staff.uta.fi>
2015-05-20 00:01:39 +0300

    "Refactored" implementations. "Refactored" in that sense that it's currently purely a prototype. Tests are kinda ok at the moment.

--- index.js
+++ index.js
@@ -4,24 +4,33 @@ var _ = require('underscore-node');
 
 module.exports =  {
   jsonToCsv: function(json, mapSpec) {
+    // This implementation is total load of bollocks and purely prototype
+    var paths = [];
+    _.each(mapSpec, function(field) {
+      if(!_.isUndefined(field.f)){
+        var path = [];
+        _.each(field.f.split("."), function(token){
+          path.push(token);
+        });
+        paths.push(path);
+      }
+    });
+
     var csvRepresentation = [];
     _.each(json, function(object) {
 
       var csvRow = [];
 
-      _.each(mapSpec, function(field) {
-
-        var t = object;
-
-        if(!_.isUndefined(field.f)){
-          _.each(field.f.split("."), function(token){
-            t = t[token];
-          })
-          if(!_.isUndefined(t)){
-            csvRow.push(t);
+      _.each(paths, function(path) {
+        var entry = object;
+        _.each(path, function(token) {
+          if(!_.isUndefined(entry)) {
+            entry = entry[token];
           }
+        });
+        if(!_.isUndefined(entry)) {
+          csvRow.push(entry);
         }
-
       });
 
       csvRepresentation.push(csvRow);
